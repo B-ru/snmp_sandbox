d6932da2d4b8f658044672a6bde71f33a368a740
Boris Ischenko <bo.ischenko@mail.ru>
2022-04-08 15:20:30 +0300

    cleaning code

--- main.c
+++ main.c
@@ -40,36 +40,42 @@
 #define OUTOFMEMORY -102
 #define FILE_IO_ERROR -103
 #define SOCKET_ERROR -104
-////////////////////
+////////////////////////////////////////
 typedef struct{
     unsigned char length;
     unsigned char top;
     unsigned char *bytes;    
 } pack_t;
-//prototypes////////
-pack_t* InitPack();
-pack_t* FormPack(pack_t*, char*, char*);
-void    AddToPack(pack_t *, unsigned char);
-unsigned char Count(char*, char);
-unsigned char* RefineOid(char*);
-void    PrintPack(pack_t *);
-void    PrintOid(unsigned char *);
-void    WriteToBin(pack_t *);
-int     CreateSocket();
-void    Request(int, char*, pack_t*);
-////////////////////
+//prototypes////////////////////////////
+pack_t*         InitPack();
+pack_t*         FormPack(pack_t*, char*, char*);
+void            AddToPack(pack_t *, unsigned char);
+unsigned char   Count(char*, char);
+unsigned char*  RefineOid(char*);
+void            PrintPack(pack_t *);
+void            PrintOid(unsigned char *);
+void            WriteToBin(pack_t *, const char* );
+int             CreateSocket();
+pack_t*         Request(int, char*, pack_t*);
+////////////////////////////////////////
+
 int main( int argc, char* argv[]){
     int socket;
-    printf("community:\t%s\noid:\t\t%s\n", argv[1], argv[2]);   
     pack_t *my_pack = InitPack();
+    pack_t *response = NULL;
     FormPack(my_pack, argv[1], argv[2]);
+    printf("request:\n");
     PrintPack(my_pack);
-    WriteToBin(my_pack);
+    //WriteToBin(my_pack,"request.bin");
     socket = CreateSocket();
-    Request(socket, argv[3], my_pack);
+    response = Request(socket, argv[3], my_pack);
+    printf("response:\n");
+    PrintPack(response);
+    //WriteToBin(response, "response.bin");
     return 0;
 }
-//functions/////////
+
+//functions////////////////////////////
 pack_t* InitPack(){
     pack_t *pack = NULL;
     pack = malloc(sizeof(pack_t));
@@ -83,11 +89,11 @@ pack_t* InitPack(){
     pack->top = 0;
     return pack;
 }
-
+////////////////////////////////////////
 pack_t* FormPack(pack_t *p_pack, char *p_community, char *p_oid){
     unsigned char *oid = malloc(256 * sizeof(unsigned char));
     oid = RefineOid(p_oid);
-    //PrintOid(oid);
+    //unsigned char *template = malloc(256 * sizeof(unsigned char));
     unsigned char sizes_[4];
     int size_counter = 0;
     AddToPack(p_pack, 48);
@@ -121,33 +127,32 @@ pack_t* FormPack(pack_t *p_pack, char *p_community, char *p_oid){
     AddToPack(p_pack, 255);
     sizes_[size_counter++] = p_pack->top - 1;
     AddToPack(p_pack, 6);
-    AddToPack(p_pack, Count(oid, 0));
-    for(int i = 0; i < Count(oid,0); i++){
+    AddToPack(p_pack, Count((char *)oid, 0));
+    for(int i = 0; i < Count((char *)oid,0); i++){
         AddToPack(p_pack, oid[i]);
     }
     AddToPack(p_pack, 5);
     AddToPack(p_pack, 0);
     for(int i = 0; i < 4; i++){
-        fprintf(stderr, "%d\t%d\n", sizes_[i], p_pack->top-2);
         p_pack->bytes[sizes_[i]] = p_pack->top - 1 - sizes_[i];
     }
     free(oid);
     return p_pack;
 }
-
+////////////////////////////////////////
 void AddToPack(pack_t *p_pack, unsigned char p_byte){
     p_pack->bytes[p_pack->top++] = p_byte;
 }
-
+////////////////////////////////////////
 unsigned char Count(char *p_string, char p_c){
     unsigned char i = 0;
-    char c = p_string[i++];
+    char c = (unsigned char)p_string[i++];
     while(c != p_c){
         c = p_string[i++];        
     }
     return i-1;
 }
-
+////////////////////////////////////////
 unsigned char* RefineOid(char *p_oid){
     char *input = malloc(256 * sizeof(char));
     strcpy(input, p_oid);
@@ -156,52 +161,48 @@ unsigned char* RefineOid(char *p_oid){
     int i = 0;
     for(; Count(input,0) ; i++){
         if(i == 0){
-            unsigned char *temp = malloc(8 * sizeof(unsigned char));
-            sscanf(input, "1.3.", &temp);
-            if(temp){
+            unsigned char temp[2];
+            sscanf(input, "%hhu.%hhu.", &temp[0],&temp[1]);
+            if(temp[0] == 1 && temp[1] == 3 ){
                 memmove(input, input + 4, Count(input,0) - 3);
                 buffer[i] = 43;
             }
             
         } else {
-            sscanf(input,"%d.",&buffer[i]);
+            sscanf(input,"%hhu.",&buffer[i]);
             unsigned char step = Count(input, '.') + 1;
             memmove(input,input + step,Count(input,0) - step + 1);
         }
     }
     buffer[++i] = 0;
-    for(int j = 0; j < i; j++){
-        printf("%02x ",  buffer[j]);
-    }
-    printf("\n");
     return buffer;
 }
-
+////////////////////////////////////////
 void PrintPack(pack_t *p_pack){
     for(int i = 0; i < p_pack->top; i++ ){
         printf("%02x ", p_pack->bytes[i]);
     }
     printf("\n");
 }
-
+////////////////////////////////////////
 void PrintOid(unsigned char *p_oid){
-    printf("%u\n", Count(p_oid, 0));
+    printf("%u\n", Count((char*)p_oid, 0));
 }
-
-void WriteToBin(pack_t *p_pack){
+////////////////////////////////////////
+void WriteToBin(pack_t *p_pack, const char *p_filename){
     FILE *outfile = NULL;
-    outfile = fopen("request.bin", "wb");
+    outfile = fopen(p_filename, "wb+x");
     if(outfile == NULL){
         printf("Error opening file\n");
         exit(FILE_IO_ERROR);
     } else {
         fwrite(p_pack->bytes, sizeof(unsigned char), p_pack->top, outfile);
+        fclose(outfile);
     }
 }
-
+////////////////////////////////////////
 int CreateSocket(){
-    int sock;
-    sock = socket(AF_INET,SOCK_DGRAM,0);
+    int sock = socket(AF_INET,SOCK_DGRAM,0);
     if(sock<0)
     {
         printf("Socket creation error!\n");
@@ -210,27 +211,20 @@ int CreateSocket(){
         return sock;
     }
 }
-
-void Request(int socket, char *address, pack_t *p_pack ){
+////////////////////////////////////////
+pack_t* Request(int socket, char *address, pack_t *p_pack ){
     unsigned char *buffer = malloc( 256 * sizeof(unsigned char));
-    int n,length;
+    unsigned int n,length;
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_port = htons(PORT);
     addr.sin_addr.s_addr = inet_addr(address);
     sendto(socket, (const char *)p_pack->bytes, p_pack->top , MSG_CONFIRM, (const struct sockaddr *) &addr, sizeof(addr));
     n = recvfrom(socket, buffer, MAXLINE, MSG_WAITALL,(struct sockaddr *) &addr, &length);
-    printf("recieved: %03d bytes\n%s\n", n, buffer);
-    ///debug
-    FILE *outfile = NULL;
-    outfile = fopen("response.bin","wb");
-     if(outfile == NULL){
-        printf("Error opening file\n");
-        exit(FILE_IO_ERROR);
-    } else {
-        fwrite(buffer, sizeof(unsigned char), n, outfile);
+    pack_t *response = InitPack();
+    for(int i = 0; i < n; i++){
+        AddToPack(response, (unsigned char)buffer[i] );
     }
-    ///
     close(socket);
-    return 
+    return response;
 }
